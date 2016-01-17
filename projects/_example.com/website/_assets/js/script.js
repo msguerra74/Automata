4b1c7a03327ff0de3baf9958d27d61cafe588649
Michael Guerra <msguerra74@gmail.com>
2016-01-16 20:34:55 -0600

    Added FastClick as a default Bower component and the appropriate JS initialization.

--- projects/_example.com/website/_assets/js/script.js
+++ projects/_example.com/website/_assets/js/script.js
@@ -3,3 +3,9 @@
 
 // jQuery
 // @import 'bower_components/jquery/dist/jquery.js';
+
+// FastClick
+// @import 'bower_components/fastclick/lib/fastclick.js';
+$(function() {
+  FastClick.attach(document.body);
+});
