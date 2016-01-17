8f4b21d893de4a218cf5ee391e4cd7bc2715feb4
Michael Guerra <msguerra74@gmail.com>
2016-01-17 01:26:00 -0600

    Added Foundation support back and updated the JS/SCSS files.

--- projects/_example.com/website/_assets/js/oldie.js
+++ projects/_example.com/website/_assets/js/oldie.js
@@ -1,5 +1,13 @@
 // OldIE JavaScript
 // ----------------
 
+// ES5 Shim/Sham
+// https://github.com/es-shims/es5-shim
+
+// @import 'bower_components/es5-shim/es5-shim.js';
+// @import 'bower_components/es5-shim/es5-sham.js';
+
 // HTML5 Shiv
+// https://github.com/afarkas/html5shiv
+
 // @import 'bower_components/html5shiv/dist/html5shiv.js';
--- projects/_example.com/website/_assets/js/script.js
+++ projects/_example.com/website/_assets/js/script.js
@@ -1,11 +1,64 @@
 // JavaScript
 // ----------
 
-// jQuery
-// @import 'bower_components/jquery/dist/jquery.js';
+// Bower Components
+// ----------------
+// To import any of the following bower_components, change '//' to '// @import'.
+
+// jQuery Legacy/Modern
+// http://api.jquery.com
+
+// 'bower_components/jquery-legacy/dist/jquery.js';
+// 'bower_components/jquery/dist/jquery.js';
+
+// Foundation
+// http://foundation.zurb.com/sites/docs/
+
+// 'bower_components/what-input/what-input.js';
+// 'bower_components/foundation-sites/js/foundation.core.js';
+// 'bower_components/foundation-sites/js/foundation.abide.js';
+// 'bower_components/foundation-sites/js/foundation.accordion.js';
+// 'bower_components/foundation-sites/js/foundation.accordionMenu.js';
+// 'bower_components/foundation-sites/js/foundation.drilldown.js';
+// 'bower_components/foundation-sites/js/foundation.dropdown.js';
+// 'bower_components/foundation-sites/js/foundation.dropdownMenu.js';
+// 'bower_components/foundation-sites/js/foundation.equalizer.js';
+// 'bower_components/foundation-sites/js/foundation.interchange.js';
+// 'bower_components/foundation-sites/js/foundation.magellan.js';
+// 'bower_components/foundation-sites/js/foundation.offcanvas.js';
+// 'bower_components/foundation-sites/js/foundation.orbit.js';
+// 'bower_components/foundation-sites/js/foundation.responsiveMenu.js';
+// 'bower_components/foundation-sites/js/foundation.responsiveToggle.js';
+// 'bower_components/foundation-sites/js/foundation.reveal.js';
+// 'bower_components/foundation-sites/js/foundation.slider.js';
+// 'bower_components/foundation-sites/js/foundation.sticky.js';
+// 'bower_components/foundation-sites/js/foundation.tabs.js';
+// 'bower_components/foundation-sites/js/foundation.toggler.js';
+// 'bower_components/foundation-sites/js/foundation.tooltip.js';
+// 'bower_components/foundation-sites/js/foundation.util.box.js';
+// 'bower_components/foundation-sites/js/foundation.util.keyboard.js';
+// 'bower_components/foundation-sites/js/foundation.util.mediaQuery.js';
+// 'bower_components/foundation-sites/js/foundation.util.motion.js';
+// 'bower_components/foundation-sites/js/foundation.util.nest.js';
+// 'bower_components/foundation-sites/js/foundation.util.timerAndImageLoader.js';
+// 'bower_components/foundation-sites/js/foundation.util.touch.js';
+// 'bower_components/foundation-sites/js/foundation.util.triggers.js';
 
 // FastClick
-// @import 'bower_components/fastclick/lib/fastclick.js';
-$(function() {
-  FastClick.attach(document.body);
-});
+// https://github.com/ftlabs/fastclick
+
+// 'bower_components/fastclick/lib/fastclick.js';
+
+// Compontent Initilalization
+// --------------------------
+// To initialize any of the following components, uncomment the function.
+
+// Foundation
+
+// $(document).foundation();
+
+// FastClick
+
+// $(function() {
+//   FastClick.attach(document.body);
+// });
