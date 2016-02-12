318ec876f2405a8bf7ff15aabf7f7cc477abbac5
Michael Guerra <msguerra74@gmail.com>
2016-02-12 12:47:08 -0600

    Cleaned things up and removed empty CSS/HTML selectors/tags.

--- projects/_example.com/website/_assets/js/script.js
+++ projects/_example.com/website/_assets/js/script.js
@@ -1,11 +1,10 @@
 // JavaScript / jQuery
 // -------------------
-// To @include any of the these scripts, add '@' before 'include'
+// '// @include()' to include any of the the following scripts
 
 // jQuery
-
-// include('../bower_components/jquery/dist/jquery.js')
+// ('../bower_components/jquery/dist/jquery.js')
 
 // Foundation for Sites
-
-// include('foundation/foundation.js')
+// Requires jQuery
+// ('foundation/foundation.js')
